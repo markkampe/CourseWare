f69d64a7b6c0b5cf3e19a41dc2781f0d6c2b6d5a
Mark.Kampe <mark.kampe@gmail.com>
2020-03-20 15:42:47 -0700

    update project references to new combined file

--- Website/swe_dates.h
+++ Website/swe_dates.h
@@ -1,44 +1,76 @@
+<P>
+This course is about software project skills other than programming
+(e.g. concept development, requirements, architecture and design, reviews,
+testing, and management).  You will be asked to form teams and come 
+up with a <a href="projects.html">single (large) project</a> concept.  
+Then, over the course of the semester, you will work on different 
+problems within that one project:
+</P>
+<center>
 <TABLE frame="box" rules="all"><TBODY>
      <col width="40">	<col width="200">	<col width="600">
-<TR> <TH>Due Date</TH>	<TH>Project</TH>	<TH>Summary of Activities</TH></TR>
-<TR> <TD>P1A_DUE</TD>	<TD><A Href="projects/proj_1.html#part1a">1a. Team, Concept, Plan</TD>
-	<TD>Form teams, identify a preliminary concept, and a plan for turning it into a proposal.</TD></TR>
-<TR> <TD>P1B_DUE</TD>	<TD><A Href="projects/proj_1.html#part1b">1b. Competitive Research</TD>
+<TR> <TH>Due Date<sup>1</sup></TH>	<TH>Assignment<sup>2</sup></TH>	<TH>Summary of Activities</TH></TR>
+<TR> <td></td><td colspan=2><strong>Phase 1: <a href="projects.html#desc_1">Concept Development, Requirements and Proposal</a></strong></td></TR>
+<TR> <TD>P1A_DUE</TD>	<TD><A Href="projects.html#desc_1a">1a. Team, Concept, Plan</TD>
+	<TD>Form teams, identify a preliminary concept, write plan for turning it into a proposal.</TD></TR>
+<TR> <TD>P1B_DUE</TD>	<TD><A Href="projects.html#desc_1b">1b. Competitive Research</TD>
 	<TD>Research existing products in this space, position your proposal against this field,
 	     and develop a competitive value proposition.</TD></TR>
-<TR> <TD>P1C_DUE</TD>	<TD><A Href="projects/proj_1.html#part1c">1c. Requirements Development</TD>
-	<TD>Identify and characterize potential users, conduct interviews to gather
-	    requirements, analyze and report on the results.</TD></TR>
-<TR> <TD>P1D_DUE</TD>	<TD><A Href="projects/proj_1.html#part1d">1d. Final Proposal</TD>
-	<TD>Combine all of the above into a complete project/product proposal (suitable 
-	    to be summitted for funding/approval).</TD></TR>
-<TR><td></td></TR>
-<TR> <TD>P2A_DUE</TD>	<TD><A Href="projects/proj_2.html#part2a">2a. Prelimnary Architecture</TD>
+<TR> <TD>P1C_DUE</TD>	<TD><A Href="projects.html#desc_1c">1c. Requirements Development</TD>
+	<TD>1. Create a concept presentation to introduce people to the type of product you are exploring<br>
+	    2. Identify and characterize potential users<br>
+	    3. conduct interviews to gather requirements<br>
+	    4. analyze and report on the results.</TD></TR>
+<TR> <TD>P1D_DUE</TD>	<TD><A Href="projects.html#desc_1d">1d. Final Proposal</TD>
+	<TD>1. Combine all of the above into a complete project/product proposal 
+	    (what will be built, why you will be successful)
+	    suitable to be summitted for funding/approval.<br>
+	    2. Review the processes you followed in this project to see what lessons you can learn
+	    for how to do things more effectively in the future.
+	    </TD></TR>
+<TR> <td></td><td colspan=2><strong>Phase 2: <a href="projects.html#desc_2">Architecture and Review</a></strong></td></TR>
+<TR> <TD>P2A_DUE</TD>	<TD><A Href="projects.html#desc_2a">2a. Prelimnary Architecture</TD>
 	<TD>Develop and refine a preliminary architecture and set of high level component specifications.</TD></TR>
-<TR> <TD>P2B_DUE</TD>	<TD><A Href="projects/proj_2.html#part2b">2b. Architecture Review</TD>
+<TR> <TD>P2B_DUE</TD>	<TD><A Href="projects.html#desc_2b">2b. Architecture Review</TD>
 	<TD>You will study and review another team's proposal, as they will study and review yours, 
 	    write up reports, and work the issues raised in those reviews.</TD></TR>
-<TR> <TD>P2C_DUE</TD>	<TD><A Href="projects/proj_2.html#part2c">2c. Final Architecture</TD>
+<TR> <TD>P2C_DUE</TD>	<TD><A Href="projects.html#desc_2c">2c. Final Architecture</TD>
 	<TD>Prepare a final architecture and set of high level component specifications
 	    that addresses the issues raised in the reviews.</TD></TR>
-<TR><td></td></TR>
-<TR> <TD>P3A_DUE</TD>	<TD><A Href="projects/proj_3.html#part3a">3a. Planning</TD>
+<TR> <td></td><td colspan=2><strong>Phase 3: <a href="projects.html#desc_3">Planning, Specification, Design and Review</a></strong></td></TR>
+<TR> <TD>P3A_DUE</TD>	<TD><A Href="projects.html#desc_3a">3a. Planning</TD>
 	<TD>Select the component(s) that you will actually design and implement, 
 	    and prepare a detailed plan for the completion that work.</TD></TR>
-<TR> <TD>P3B_DUE</TD>	<TD><A Href="projects/proj_3.html#part3b">3b. Component Specifications</TD>
+<TR> <TD>P3B_DUE</TD>	<TD><A Href="projects.html#desc_3b">3b. Component Specifications</TD>
 	<TD>Elaborate the architecture above the chosen compoents and produce a set
 	    of complete functional specifications for them.</TD></TR>
-<TR> <TD>P3C_DUE</TD>	<TD><A Href="projects/proj_3.html#part3c">3c. Component Design and Test Plan</TD>
+<TR> <TD>P3C_DUE</TD>	<TD><A Href="projects.html#desc_3c">3c. Component Design and Test Plan</TD>
 	<TD>Prepare a detailed design and unit-testing plan for each of the chosen components.</TD></TR>
-<TR> <TD>P3D_DUE</TD>	<TD><A Href="projects/proj_3.html#part3d">3d. Design Review </TD>
+<TR> <TD>P3D_DUE</TD>	<TD><A Href="projects.html#desc_3d">3d. Design Review </TD>
 	<TD>You will study and review another team's designs, as they will study and review yours,
 	    write up reports, and work the issues raised in those reviews.</TD></TR>
-<TR> <TD>P3E_DUE</TD>	<TD><A Href="projects/proj_3.html#part3e">3e. Final Design</TD>
+<TR> <TD>P3E_DUE</TD>	<TD><A Href="projects.html#desc_3e">3e. Final Design</TD>
 	<TD>Write up the final form specifications and test plan for the components to
 	    be implemented.</TD></TR>
-<TR><td></td></TR>
-<TR> <TD>P4_DUE</TD>	<TD><A Href="projects/proj_4.html">4. Final Report</TD>
+<TR> <td></td><td colspan=2><strong>Phase 4: <a href="projects.html#desc_4">Implementation and Testing Sprint</a></strong></td></TR>
+<TR> <TD>P4_DUE</TD>	<TD><A Href="projects.html#desc_4">4. Final Report</TD>
 	<TD>Implement the chosen components and test plans (including pair programming
 	    and code reviews) and present a brief <em>Sprint Review</em> of the
 	    completed work.</TD></TR>
 </TBODY></TABLE>
+</center>
+
+<ul>
+	<sup>1</sup> Submissions are due by midnight at the end of the due date.<br>
+	<sup>2</sup> Unless otherwise specified, all written materials should be 
+		<a href="projects.html#submission">submitted</a>
+ 		(via e-mail) containing URLs to ASCII text files in a repo on 
+		<a href="https://github.com">Github</a>.
+	<ul>
+		Work-products will be considered to have been submitted at the time of the last
+		<em>commit</em> and <em>push</em>.<br>
+	</ul>
+	<P>
+	<a href="projects.html#grading">General comments on project rules and grading</a> can be 
+	found at the end of the project descriptions.
+</ul>
