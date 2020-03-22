65988189126316c89faea46b97c1efe53666d6a3
Mark.Kampe <mark.kampe@gmail.com>
2020-03-22 14:05:16 -0700

    move QandA.html back into info subdir

--- Website/index.c
+++ Website/index.c
@@ -42,7 +42,7 @@ to their solution.
 	<LI> <strong>Office Hours</strong>:
 #include "office_hours.h"
 	    </LI>
-	<LI> <A href="QandA.html">Frequently Asked Questions</A> </LI>
+	<LI> <A href="info/QandA.html">Frequently Asked Questions</A> </LI>
 </UL>
 
 
