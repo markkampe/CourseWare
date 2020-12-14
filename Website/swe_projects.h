795aab33144f6b12226da1f98c9313f7b1390a3f
Mark.Kampe <mark.kampe@gmail.com>
2020-12-14 13:42:21 -0800

    take section #s out of the hrefs
    	to make it easier to shuffle tasks between weeks

--- Website/swe_projects.h
+++ Website/swe_projects.h
@@ -11,17 +11,17 @@ aspects of that one project:
      <col width="40">	<col width="200">	<col width="600">
 <TR> <TH>Due Date<sup>1</sup></TH>	<TH>Assignment<sup>2</sup></TH>	<TH>Summary of Activities</TH></TR>
 <TR> <td colspan=3><strong>Phase 1: <a href="projects.html#desc_1">Concept Development, Requirements and Proposal</a></strong></td></TR>
-<TR> <TD>P1A_DUE</TD>	<TD><A Href="projects.html#desc_1a">1a. Team, Concept, Plan</TD>
+<TR> <TD>P1A_DUE</TD>	<TD><A Href="projects.html#concept">1a. Team, Concept, Plan</TD>
 	<TD>Form teams, identify a preliminary concept, write plan for turning it into a proposal.</TD></TR>
-<TR> <TD>P1B_DUE</TD>	<TD><A Href="projects.html#desc_1b">1b. Competitive Research</TD>
+<TR> <TD>P1B_DUE</TD>	<TD><A Href="projects.html#compet">1b. Competitive Research</TD>
 	<TD>Research existing products in this space, position your proposal against this field,
 	     and develop a competitive value proposition.</TD></TR>
-<TR> <TD>P1C_DUE</TD>	<TD><A Href="projects.html#desc_1c">1c. Requirements Development</TD>
+<TR> <TD>P1C_DUE</TD>	<TD><A Href="projects.html#reqts">1c. Requirements Development</TD>
 	<TD>1. Create a concept presentation to introduce people to the type of product you are exploring<br>
 	    2. Identify and characterize potential users<br>
 	    3. conduct interviews to gather requirements<br>
 	    4. analyze and report on the results.</TD></TR>
-<TR> <TD>P1D_DUE</TD>	<TD><A Href="projects.html#desc_1d">1d. Final Proposal</TD>
+<TR> <TD>P1D_DUE</TD>	<TD><A Href="projects.html#proposal">1d. Final Proposal</TD>
 	<TD>1. Combine all of the above into a complete project/product proposal 
 	    (what will be built, why you will be successful)
 	    suitable to be summitted for funding/approval.<br>
@@ -29,21 +29,22 @@ aspects of that one project:
 	    for how to do things more effectively in the future.
 	    </TD></TR>
 <TR> <td colspan=3><strong>Phase 2: <a href="projects.html#desc_2">Architecture and Review</a></strong></td></TR>
-<TR> <TD>P2A_DUE</TD>	<TD><A Href="projects.html#desc_2a1">2a. Plan and Prelimnary Architecture</TD>
+<TR> <TD>P2A_DUE</TD>	<TD>2a. <A Href="projects.html#desc_plan2">Plan</A> and
+		<A Href="projects.html#desc_arch_prelim">Prelimnary Architecture</A></TD>
 	<TD>1. Develop a plan for this project, dividing the work over
 	       your team members and the available time.<br>
 	    2. Develop and document an architecture and high-level component specifications 
 	       for your project.  This includes doing any required research and prototyping to
 	       address critical questions.
 	</TD></TR>
-<TR> <TD>P2B_DUE</TD>	<TD><A Href="projects.html#desc_2b">2b. Architecture Review</TD>
+<TR> <TD>P2B_DUE</TD>	<TD><A Href="projects.html#desc_arch_review">2b. Architecture Review</TD>
 	<TD>1. Study another team's architecture, and prepare notes for a design review,
 	       as they will study and pepare for a review of yours.<br>
 	    2. Conduct a design review with that other team, as they will with you.<br>
 	    3. Write up a report of the review
 	    4. Continue to work any issues with the team that raised them.
 	</TD></TR>
-<TR> <TD>P2C_DUE</TD>	<TD><A Href="projects.html#desc_2c">2c. Final Architecture</TD>
+<TR> <TD>P2C_DUE</TD>	<TD><A Href="projects.html#desc_arch_final">2c. Final Architecture</TD>
 	<TD>1. Revise your preliminary architecture based on the results of your review and investigations,
 	       and submit a report on the identified issues and their resolutions.<br>
 	    2. Prepare and submit a final architectural proposal.<br>
@@ -51,7 +52,7 @@ aspects of that one project:
 	    for how to do things more effectively in the future.
 	</TD></TR>
 <TR> <td colspan=3><strong>Phase 3: <a href="projects.html#desc_3">Component Specifications, Design and Test Plan</a></strong></td></TR>
-<TR> <TD>P3A_DUE</TD>	<TD>3a. <A Href="projects.html#desc_3a">Component Selection and Specifications</TD>
+<TR> <TD>P3A_DUE</TD>	<TD>3a. <A Href="projects.html#desc_spec">Component Selection and Specifications</TD>
 	<TD>1. Select the system components to be designed and implmented<br>
 	    2. Select an owner for each component<br>
 	    3. Develop a schedule and assign responsibilities for the remaing phases<br>
@@ -59,18 +60,19 @@ aspects of that one project:
 	        develop detailed specfiications for each.<br>
 	    5. As individuals, write up detailed specifications for your assigned component.
         </TD></TR>
-<TR> <TD>P3B_DUE</TD>	<TD>3b. <A Href="projects.html#desc_3b1">Component Design</a> and 
-				<A Href="projects.html#desc_3b2">Test Plan</a></TD>
+<TR> <TD>P3B_DUE</TD>	<TD>3b. <A Href="projects.html#desc_dsgn">Component Design</a> and 
+				<A Href="projects.html#desc_test">Test Plan</a></TD>
 	<TD> 1. As individuals, prepare a detailed design for your assigned component.<br>
 	     2. As individuals, prepare detailed unit-testing plans for your assigned component.
 	</TD></TR>
-<TR> <TD>P3C_DUE</TD>	<TD>3c. Design Review <A Href="projects.html#desc_3c1">notes</a> and 
-					      <A Href="projects.html#desc_3c2">report</a>.</TD>
+<TR> <TD>P3C_DUE</TD>	<TD>3c. Design Review <A Href="projects.html#desc_notes3">notes</a>,
+				meeting and <A Href="projects.html#desc_report3">report</a>.</TD>
 	<TD> 1. As individuals, package your specifications, design and test plans for review.</br>
 	     2. As individuals, read each package and prepare notes.</br>
 	     3. As a group, review each package, and write a report for each review.
 	</TD></TR>
-<TR> <TD>P3D_DUE</TD>	<TD><A Href="projects.html#desc_3d">3d. Final Design</TD>
+<TR> <TD>P3D_DUE</TD>	<TD>3d. <A Href="projects.html#desc_final3">
+			    Final Specification, Design, and Test Plan</A></TD>
 	<TD> 1. As individuals, revise your specifications, designs, and test plans
 	        to address all important issues raised in the review.<br>
 	     2. As a team, review the processes you have followed, and write up a
@@ -78,19 +80,29 @@ aspects of that one project:
 	</TD></TR>
 <TR> <td colspan=3><strong>Phase 4: <a href="projects.html#desc_4">Implementation and Testing Sprint</a></strong></td></TR>
 <TR> <TD>P4D_DUE</TD>	<TD><A Href="projects.html#desc_4">4. Final Reports</TD>
-	<TD> 1. Each of you will implement the component you designed in phase 3.<br>
-	     2. Two of you will spend (at least) one session doing <em>pair-programming</em>.<br>
-	     3. (at least) one of you will develop your code in a <em>Test Driven Development</em>
-	        fashion (implementing and running tests as you complete each routine or feature).<br>
+	<TD> 1. Each of you will 
+		<A Href="projects.html#desc_final_code">implement the component</A>
+		you designed in phase 3.<br>
+	     2. Two of you will spend (at least) one session doing 
+	        <A Href="projects.html#desc_pairpgm">pair-programming</A>
+		and write up a brief report on the experience.<br>
+	     3. (at least) one of you will develop your code in a
+	     	<A Href="projects.html#desc_tdd">Test Driven Development</A>
+	        fashion (implementing and running tests as you complete each routine or feature)
+		and write up a brief report on your experience.<br>
 	     4. (at least) one of you will implement your component and then submit it to 
-	        <em>code review</em> by other members of your team.<br>
-	     5. Each of you will implement the component test plan you designed (in phase 3)
+	        <A Href="projects.html#desc_review4">code review</A>
+		by other members of your team and produce a report from that review.<br>
+	     5. Each of you will implement the 
+		<A Href="projects.html#desc_final_test">component test plan</A>
+		you designed (in phase 3)
 	        and use it to validate the correctness of your implementation.<br>
-	     6. As a team, design a demo that shows your (independently implemented)
-	        components working together.<br>
-	     7. As a team, prepare and present a brief sprint review.<br>
+	     6. As a team, design a 
+	     	<A Href="projects.html#desc_demo4">demo</A>
+		that shows your (independently implemented) components working together.<br>
+	     7. As a team, prepare and present a brief sprint review and demo.<br>
 	     8. As a team, review the processes you have followed, and write up a
-	        post-mortem report.
+	        <A Href="projects.html#desc_4pm">post-mortem report</A>.
 	</TD></TR>
 </TBODY></TABLE>
 </center>
