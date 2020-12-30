743ba37ef6a8beac84b3f0db3dccc4ecff1d39c2
Mark.Kampe <mark.kampe@gmail.com>
2020-12-30 15:31:49 -0800

    push project images as well as html

--- Website/push.sh
+++ Website/push.sh
@@ -13,11 +13,11 @@ echo "updating HTML course information on $DEST"
 scp index.html schedule.html syllabus.html keys.html FAQ.html $DEST
 scp slides.html $DEST/slides/index.html
 
-PROJECTS=../../Big-Software/projects/projects.html
-if [ -r $PROJECTS ]
+PROJECTS=../../Big-Software/projects
+if [ -r $PROJECTS/projects.html ]
 then
     echo "... including latest descriptions from $PROJECTS"
-    scp $PROJECTS $DEST
+    scp $PROJECTS/projects.html $PROJECTS/proj_?.jpg $DEST
 else
     echo "... unable to find an updated $PROJECTS"
 fi
