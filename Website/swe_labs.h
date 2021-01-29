8b15a708c7de04478da3a3521a90239d7a13df65
Mark.Kampe <mark.kampe@gmail.com>
2021-01-29 11:57:02 -0800

    (initial) hand-generated ToC for lab sessions

---
+++ Website/swe_labs.h
@@ -0,0 +1,30 @@
+<a name="lab_sessions"></a>
+<!-- TODO: This should be auto-generated from SCHED.csv -->
+There are (with a few exceptions) no planned presentations or exercises
+for the lab sessions.  Rather, they are an opportunity for us all to
+get together a few days before the deliverables are due, to ensure that
+you have what you need and know what you have to do with it.
+Each weekly lab session is focused on that week's project deliverables.
+The following slides outine the goals for each lab session:
+<OL type="1">
+   <li> <A href="lab_1a.html">P1A: Teams, Concepts and Plans</a> </li>
+   <li> <A href="lab_1b.html">P1B: Competitive Research</a> </li>
+   <li> <A href="lab_1c.html">P1C: Requirements Development</a> </li>
+   <li> <A href="lab_1d.html">P1D: Final Proposal</a> </li>
+   <li> <A href="lab_2a.html">P2A: Preliminary Architecture</a> </li>
+   <li> Spring Break </li>
+   <li> <A href="lab_2c.html">P2C: Final Architecture</a> </li>
+   <li> Cesar Chavez Day </li>
+   <li> <A href="lab_3b.html">P3B: Component Design and Test Plan</a> </li>
+   <li> <A href="lab_3c.html">P3C: Component Design Reviews</a> </li>
+   <li> <A href="lab_3d.html">P3D: Final Component Design and Test Plan</a> </li>
+   <li> <A href="lab_4a.html">P4A: Implementation</a> and <A href="lab_4b.html">P4B: Testing</A></li>
+   <li> <A href="lab_4c.html">P4C: Integration and Demo Preparation</a> </li>
+   <li> <A href="lab_4d.html">P4D: Demo and Post-Mortem</a> </li>
+</OL>
+<P>
+<!-- TODO: This URL should come from courseinfo.h -->
+To schedule time working with individual teams, please add your team
+and a time to the
+<A Href="https://docs.google.com/spreadsheets/d/1qHE-BNpciJUCfKEfkbU_NHKs3dKgATBC1XLBaItuLMA/edit?usp=sharing">Lab sign-up sheet</A>
+</P>
