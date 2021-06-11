c12e66d6c97b1e51fe0cb70fc9e79e0cb02471f0
Mark.Kampe <mark.kampe@gmail.com>
2021-06-10 18:34:48 -0700

    add URLs for Fall21

--- Website/courseinfo.h
+++ Website/courseinfo.h
@@ -28,7 +28,6 @@
 #define	SCORE_P4	12.5%
 
 #define SLIP_DAYS 4
-#define REMOTE_EXAMS	TRUE
 #endif  SWE
 
 #ifdef UCLA
@@ -38,14 +37,22 @@
 #define NUMBER CS181F
 #endif  POM
 #ifdef HMC
-#if YEAR == 2021
-#define NUMBER CS181AA
-#else
+#if YEAR <= 2020
 #define NUMBER CS121
+#else
+#define NUMBER CS181AA
 #endif YEAR
+#if YEAR == 2021 && MONTH < 9
 #define ZOOM    "https://hmc-edu.zoom.us/j/93895586571?pwd=bUlYNlRGT2ZRajRtY0tKU0gxK3QwZz09"
-#define PIAZZA  "https://piazza.com/claremont/spring2021/hmcsci181aa/home"
+#define PIAZZA  "https://piazza.com/claremont/2021/hmcsci181aa/home"
 #define GRADESCOPE "https://gradescope.com/courses/214146"
+#define REMOTE_EXAMS	TRUE
+#endif	Spring 2021
+#if YEAR == 2021 && MONTH >= 9
+#define PIAZZA  "https://piazza.com/claremont/fall2021/csci181aa/home"
+#define GRADESCOPE "https://gradescope.com/courses/274176"
+#define GRADESCOPE_CODE KYB7W5
+#endif	Fall 2021
 #endif HMC
 
 #ifdef UCLA
