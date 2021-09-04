0c6c1009df520dc8726bc0a19a8778435a17ca5e
Mark.Kampe <mark.kampe@gmail.com>
2021-09-03 20:48:56 -0700

    re-organized and indented to improve readability/maintainability

--- Website/courseinfo.h
+++ Website/courseinfo.h
@@ -1,47 +1,66 @@
 #if MONTH >= 9
-#define SEASON Fall
+#    define SEASON Fall
 #else
-#define SEASON Spring
+#    define SEASON Spring
 #endif
 
+/* OS course parameters	*/
 #ifdef OS
-#define COURSE Operating Systems Principles
-#ifdef UCLA
-#define NUMBER CS111
-#endif  UCLA
-#ifdef POM
-#define NUMBER CS134
-#define STUDENT_PRES
-#endif  POM
-#define SLIP_DAYS 5
+#    define COURSE Operating Systems Principles
+#    define SCORE_PROJ	50
+#    define SCORE_P4	15
+#    define SCORE_EXAM	40
+#    define SCORE_X1	15
+#    define SCORE_X2	15
+#    define SCORE_X3	10
+#    ifdef UCLA
+#	define NUMBER CS111
+#    endif  UCLA
+#
+#    ifdef POM
+#	define NUMBER CS134
+#	define STUDENT_PRES
+#    endif  POM
+#
+#    ifdef HMC
+#	define NUMBER CS134
+#	define STUDENT_PRES
+#    endif  HMC
+#    define SLIP_DAYS 5
 #endif  OS
 
+/* Software Engineering course parameters */
 #ifdef SWE
-#define COURSE Software Engineering
-#define SCORE_QUIZ	10%
-#define SCORE_PART      0
-#define SCORE_X1	20%
-#define SCORE_X2	20%
-#define	SCORE_P1	12.5%
-#define	SCORE_P2	12.5%
-#define	SCORE_P3	12.5%
-#define	SCORE_P4	12.5%
-
-#define SLIP_DAYS 4
+#    define COURSE Software Engineering
+#    define SCORE_QUIZ	10%
+#    define SCORE_PART      0
+#    define SCORE_X1	20%
+#    define SCORE_X2	20%
+#    define	SCORE_P1	12.5%
+#    define	SCORE_P2	12.5%
+#    define	SCORE_P3	12.5%
+#    define	SCORE_P4	12.5%
+#
+#    ifdef UCLA
+#	define NUMBER CS134
+#    endif
+#
+#    ifdef POM
+#	define NUMBER CS181F
+#    endif  POM
+#
+#    ifdef HMC
+#	if YEAR <= 2020
+#	    define NUMBER CS121
+#	else
+#	    define NUMBER CS181AA
+#	endif YEAR
+#    endif HMC
+#
+#    define SLIP_DAYS 4
 #endif  SWE
 
-#ifdef UCLA
-#define NUMBER CS134
-#endif  UCLA
-#ifdef POM
-#define NUMBER CS181F
-#endif  POM
-#ifdef HMC
-#if YEAR <= 2020
-#define NUMBER CS121
-#else
-#define NUMBER CS181AA
-#endif YEAR
+/* semester-specific web-site references */
 #if YEAR == 2021 && MONTH < 9
 #define ZOOM    "https://hmc-edu.zoom.us/j/93895586571?pwd=bUlYNlRGT2ZRajRtY0tKU0gxK3QwZz09"
 #define PIAZZA  "https://piazza.com/claremont/2021/hmcsci181aa/home"
@@ -54,18 +73,17 @@
 #define GRADESCOPE "https://gradescope.com/courses/274176"
 #define GRADESCOPE_CODE KYB7W5
 #endif	Fall 2021
-#endif HMC
 
+/* school-specfic submission mechanisms	*/
 #ifdef UCLA
-#define SUBMIT CCLE
-#define SUBMIT_URL "http://ccle.ucla.edu"
+#    define SUBMIT CCLE
+#    define SUBMIT_URL "http://ccle.ucla.edu"
 #endif UCLA
 #ifdef POM
-#define	SUBMIT submit.cs.pomona.edu
-#define SUBMIT_URL "http://submit.cs.pomona.edu"
+#    define	SUBMIT submit.cs.pomona.edu
+#    define SUBMIT_URL "http://submit.cs.pomona.edu"
 #endif  POM
 #ifdef HMC
-#define	SUBMIT Sakai Assignments
-#define SUBMIT_URL "http://sakai.claremont.edu"
+#    define	SUBMIT Sakai Assignments
+#    define SUBMIT_URL "http://sakai.claremont.edu"
 #endif  HMC
-
