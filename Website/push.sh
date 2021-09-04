4595a4375bf00b907da5644ca286535caaf8a8f0
Mark.Kampe <mark.kampe@gmail.com>
2021-09-03 20:48:02 -0700

    started support for HMC CS134

--- Website/push.sh
+++ Website/push.sh
@@ -10,24 +10,45 @@ YEAR=21
 
 TOP_FILES="index.html schedule.html syllabus.html keys.html FAQ.html"
 
-DEST=$ACCOUNT:$DIR/$COURSE.$SEMESTER$YEAR
+if [ -z "$1" ]
+then
+    DEST=$ACCOUNT:$DIR/$COURSE.$SEMESTER$YEAR
+else
+    DEST=$ACCOUNT:$DIR/$1
+    COURSE=`echo $1 | cut -d. -f1`
+fi
 
 echo "... updating HTML course information on $DEST"
 scp $TOP_FILES $DEST
 
-echo "... updating slides indices in $DEST/slides"
+echo "... updating lecture slides indices in $DEST/slides"
 scp slides.html $DEST/slides/index.html
-scp swe_exercises.html $DEST/slides/exercises.html
 
-# update the project descriptions
-PROJECTS=../../Big-Software/projects
-if [ -r $PROJECTS/projects.html ]
+if [[ "$COURSE" == "cs181aa" ]]
 then
-    echo "... including latest descriptions from $PROJECTS"
-    scp $PROJECTS/projects.html $PROJECTS/proj_?.jpg $DEST
-else
-    echo "... unable to find an updated $PROJECTS"
+	echo "... updating exercise slides indices in $DEST/slides"
+	scp swe_exercises.html $DEST/slides/exercises.html
+
+	# update the project descriptions
+	PROJECTS=../../Big-Software/projects
+	if [ -r $PROJECTS/projects.html ]
+	then
+	    echo "... including latest descriptions from $PROJECTS"
+	    scp $PROJECTS/projects.html $PROJECTS/proj_?.jpg $DEST
+	else
+	    echo "... unable to find an updated $PROJECTS"
+	fi
+
+	echo "... updating lab session objectives from $PROJECTS/Slides"
+	scp $PROJECTS/Slides/*.html $DEST/slides
 fi
 
-echo "... updating lab session objectives from $PROJECTS/Slides"
-scp $PROJECTS/Slides/*.html $DEST/slides
+if [[ "$COURSE" == "cs134" ]]
+then
+	# update the project descriptions
+	PROJECTS=../../OS_projects
+	for p in P0_warmup P1A_termio P1B_compress P2A_lock P2B_contention P3A_fs P3B_analyze P4A_bringup P4B_sensors P4C_iot
+	do
+		scp $PROJECTS/$p/ASSIGNMENT/* $DEST/projects
+	done
+fi
