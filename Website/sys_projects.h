9910eb5a540cc4a734802063cc97668905cd6da4
Mark.Kampe <mark.kampe@gmail.com>
2023-03-29 09:24:31 -0700

    add table for 105 projects

---
+++ Website/sys_projects.h
@@ -0,0 +1,18 @@
+<TABLE><TBODY>
+<TR> <TH>Due</TH>		<TH>Project</TH></TR>
+<TR> <TD>P1_DUE</TD>	<TD><A Href="projects/bits.pdf">Bit-Level operations and 2's complement arithmetic</A></TD>
+     </TR>
+<TR> <TD>P2_DUE</TD>	<TD><A Href="projects/debugger.pdf">Using GDB</A></TD>
+     </TR>
+<TR> <TD>P3_DUE</TD>	<TD><A Href="projects/bomb.pdf">Understanding optimized C code</A></TD>
+     </TR>
+<TR> <TD>P4_DUE</TD>	<TD><A Href="projects/attack.pdf">Understanding stack frames and machine langauge</A></TD>
+     </TR>
+<TR> <TD>P5_DUE</TD>	<TD><A Href="projects/ringbuf.pdf">Syncronizing circular buffer use</A></TD>
+     </TR>
+<TR> <TD>P6_DUE</TD>	<TD><A Href="projects/fgrep.pdf">Implement the fgrep utility</A></TD>
+     </TR>
+<TR> <TD>P7_DUE</TD>	<TD><A Href="projects/network.pdf">Implement a simple web server</A></TD>
+     </TR>
+</TBODY></TABLE>
+
